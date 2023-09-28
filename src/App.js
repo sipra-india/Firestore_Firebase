f8aba86a939a8ab2e6aa9fd14324338eb10a3cf1
Sipra <130334601+sipra-india@users.noreply.github.com>
2023-09-28 11:15:48 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+import SignIn from "./SignIn";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h2>Firestore: reading and writing</h2>
+      <hr />
+      <SignIn />
+    </div>
+  );
+}
